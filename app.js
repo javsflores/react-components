3bc3be1b28520f406f5f682ff8da26ae3db9477b
Owen Wexler <owenbwexler@gmail.com>
2020-08-31 19:37:07 -0400

    Refactor GroceryListItem to stateful and GroceryList to stateless

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
-class GroceryList extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
+const GroceryList = (props) => {
+  return (
+    <div>
       <ul>
         <GroceryListItem item="Apples" quantity="5" unit="apples" />
         <hr></hr>
@@ -12,9 +8,10 @@ class GroceryList extends React.Component {
         <hr></hr>
         <GroceryListItem item="Kale" quantity="3" unit="stalks"/>
       </ul>
-    );
-  }
+    </div>
+  );
 }
+
 
 /*
   props: {
@@ -23,8 +20,18 @@ class GroceryList extends React.Component {
     unit: 'package'
   }
 */
-const GroceryListItem = (props) => {
-  return <li>{props.item}: {props.quantity} {props.unit}</li>;
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      style: 'regular';
+    }
+  }
+
+  render() {
+    return <li style={this.state.style}>{this.props.item}: {this.props.quantity} {this.props.unit}</li>;
+  }
 }
 
 ReactDOM.render(<GroceryList/>, document.getElementById('app'));
