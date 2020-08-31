fcf2ab76fe35d4e89b63bf0d7be96e41fccb2025
Owen Wexler <owenbwexler@gmail.com>
2020-08-31 19:09:09 -0400

    Implement GroceryList and GroceryListItem classes

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-// TODO
+class GroceryList extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <ul>
+        <GroceryListItem item="Apples" quantity="5" unit="apples" />
+        <hr></hr>
+        <GroceryListItem item="Bananas" quantity="7" unit="bunches"/>
+        <hr></hr>
+        <GroceryListItem item="Kale" quantity="3" unit="stalks"/>
+      </ul>
+    );
+  }
+}
+
+/*
+  props: {
+    item: 'Kale',
+    quantity: '3'
+    unit: 'package'
+  }
+*/
+const GroceryListItem = (props) => {
+  return <li>{props.item}: {props.quantity} {props.unit}</li>;
+}
+
+ReactDOM.render(<GroceryList/>, document.getElementById('app'));
+
