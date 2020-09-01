7c5cae068fb9bb9870d46fe2883b65379d2505a1
Javier Flores <javier.floresapodaca@gmail.com>
2020-08-31 19:09:38 -0700

    Mini Sprint

--- app.js
+++ app.js
@@ -1,38 +1,31 @@
-const GroceryList = (props) => {
-  return (
+var GroceryList = (props) => {
+  return(
     <div>
-      <ul>
-        <GroceryListItem item="Apples" quantity="5" unit="apples" />
-        <hr></hr>
-        <GroceryListItem item="Bananas" quantity="7" unit="bunches"/>
-        <hr></hr>
-        <GroceryListItem item="Kale" quantity="3" unit="stalks"/>
-      </ul>
+        <GroceryListItem items={['Apples', 'Bananas', 'Kale']}/>
     </div>
+  )
+}
+
+var GroceryListItem = (props) => {
+
+  function bold(e) {
+    e.target.style.fontWeight = 900;
+  }
+
+  function normal(e) {
+    e.target.style.fontWeight = 400;
+  }
+
+  return(
+    <ul>
+      <li onMouseEnter={bold} onMouseLeave={normal}>{props.items[0]}</li>
+      <li onMouseEnter={bold} onMouseLeave={normal}>{props.items[1]}</li>
+      <li onMouseEnter={bold} onMouseLeave={normal}>{props.items[2]}</li>
+    </ul>
   );
 }
 
-
-/*
-  props: {
-    item: 'Kale',
-    quantity: '3'
-    unit: 'package'
-  }
-*/
-class GroceryListItem extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      style: 'regular';
-    }
-  }
-
-  render() {
-    return <li style={this.state.style}>{this.props.item}: {this.props.quantity} {this.props.unit}</li>;
-  }
-}
-
-ReactDOM.render(<GroceryList/>, document.getElementById('app'));
-
+ReactDOM.render(
+  <GroceryList/>,
+  document.getElementById('app')
+)
